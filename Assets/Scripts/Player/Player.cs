d4b8d24c635ebe8b914f46d8073d6620a35308f8
Bryan Mejia <bamxmejia@gmail.com>
2022-08-27 17:36:35 -0400

    Added test palettes in a grid layout to test movement. Created an unsightly checkerboard pattern to see the grid in respect to the player.  Player does in fact move in a grid like pattern that matches their size.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -16,8 +16,8 @@ namespace Player
         private Rigidbody2D body;
         private Vector3 size;
 
-        private ListSet<KeyCode> movementInputs; // Stored movement key inputs in the order they were pressed
-        private Direction bufferDir; // Direction the buffer is looking
+        private ListSet<KeyCode> movementQueue; // Stored movement key inputs in the order they were pressed
+        private Direction bufferDir; // Direction the buffer point is looking
         private Direction direction; // Current direction of the player
         private IDictionary<KeyCode, Direction> movementKeyBind;
         private KeyCode[] movementKeys;
@@ -27,7 +27,7 @@ namespace Player
         {
             body = GetComponent<Rigidbody2D>();
             size = GetComponent<BoxCollider2D>().bounds.size;
-            movementInputs = new();
+            movementQueue = new();
             movePoint.parent = null;
             bufferPoint.parent = null;
 
@@ -40,23 +40,29 @@ namespace Player
         // Update is called once per frame
         void Update()
         {
-            AddPressedMovementKeys(this.movementKeys, this.movementInputs);
-            RemoveReleasedMovementKeys(this.movementKeys, this.movementInputs);
+            // Stores pressed down movement keys into a queued set
+            AddPressedMovementKeys(this.movementKeys, this.movementQueue);
+            // Removes movement keys from queued set after they are released
+            RemoveReleasedMovementKeys(this.movementKeys, this.movementQueue);
 
-            int lastIndex = this.movementInputs.Count - 1;
+            // Acquire the last pressed down key within a set of pressed down keys
+            int lastIndex = this.movementQueue.Count - 1;
             if (lastIndex >= 0)
             {
-                Direction newDir = getDirection(this.movementInputs[lastIndex], this.movementKeyBind);
+                Direction newDir = getDirection(this.movementQueue[lastIndex], this.movementKeyBind);
                 if (newDir.GetOppositeDirection() != this.direction)
                     this.bufferDir = newDir;
             } 
 
+            // Moves the player body towards the movePoint
             body.transform.position = Vector2.MoveTowards(body.transform.position, movePoint.position, speed * Time.deltaTime);
+            // After the player reaches the movePoint, it gets moved to the location of the bufferPoint
             if (Vector2.Distance(body.transform.position, movePoint.position) <= 0f)
             {
                 movePoint.position = bufferPoint.position;
                 this.direction = this.bufferDir;
             }
+            // The bufferPoint gets moved to a player width distance from the movePoint in the player selected direction
             bufferPoint.position = movePoint.position + GetPosDelta(this.bufferDir, this.size);
 
             TestUtil.timedLog(String.Format("Current direction: {0}", this.bufferDir));
@@ -113,38 +119,38 @@ namespace Player
         /// </summary>
         /// <param name="dir">Current travel direction the player wishes to travel</param>
         /// <param name="size">The hitbox size of the player which will be used to move the player in a gridlike pattern</param>
-        /// <returns>A vector that respresents the current velocity of the player</returns>
+        /// <returns>A vector that respresents the current delta position of the buffer point</returns>
         private Vector3 GetPosDelta(Direction dir, Vector2 size)
         {
-            float xVel, yVel;
+            float xDelta, yDelta;
             switch (dir)
             {
                 case Direction.NORTH:
-                    xVel = 0;
-                    yVel = size.y;
+                    xDelta = 0;
+                    yDelta = size.y;
                     break;
                 case Direction.SOUTH:
-                    xVel = 0;
-                    yVel = -size.y;
+                    xDelta = 0;
+                    yDelta = -size.y;
                     break;
                 case Direction.EAST:
-                    xVel = size.x;
-                    yVel = 0;
+                    xDelta = size.x;
+                    yDelta = 0;
                     break;
                 case Direction.WEST:
-                    xVel = -size.x;
-                    yVel = 0;
+                    xDelta = -size.x;
+                    yDelta = 0;
                     break;
                 case Direction.NONE:
-                    xVel = 0;
-                    yVel = 0;
+                    xDelta = 0;
+                    yDelta = 0;
                     break;
                 default:
                     throw new ArgumentException(String.Format(
                         "The entered direction \"{0}\" does not have an implemented velocity", dir));
             }
 
-            return new Vector2(xVel, yVel);
+            return new Vector2(xDelta, yDelta);
         }
 
     }
